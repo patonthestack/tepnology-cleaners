679b993678bda9db7077a633d2f726ca56148969
Patrick Tep <patricktep@patricks-mbp.mynetworksettings.com>
2025-04-26 23:12:58 -0400

    revert back to NEXT_PUBLIC_API_URL and add env var in vercel injecting vercel url

--- app/config/constants.ts
+++ app/config/constants.ts
@@ -1,6 +1,1 @@
-export const BASE_URL =
-	process.env.VERCEL_ENV === 'production' ||
-	process.env.VERCEL_ENV === 'preview' ||
-	process.env.VERCEL_ENV === 'development'
-		? `https://${process.env.VERCEL_URL}`
-		: 'http://localhost:3000';
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
