c1863c959b2f2832347cec1bb1e08a4312796b7c
Patrick Tep <patricktep@patricks-mbp.mynetworksettings.com>
2025-04-26 23:26:11 -0400

    add https to vercel_url

--- app/config/constants.ts
+++ app/config/constants.ts
@@ -1,1 +1,6 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL;
+export const BASE_URL =
+	process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ||
+	process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview' ||
+	process.env.NEXT_PUBLIC_VERCEL_ENV === 'development'
+		? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+		: 'http://localhost:3000';
