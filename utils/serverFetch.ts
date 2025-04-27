3f2fa803018ca76bebe5af37efd83912a09f7503
Patrick Tep <patricktep@patricks-mbp.mynetworksettings.com>
2025-04-26 22:55:20 -0400

    use vercel_url over next_public_api_url

--- utils/serverFetch.ts
+++ utils/serverFetch.ts
@@ -1,5 +1,6 @@
 export default async function serverFetch(path: string, options = {}) {
-	const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+	const baseUrl =
+		`https://${process.env.VERCEL_URL}` || 'http://localhost:3000';
 	const url = new URL(path, baseUrl);
 	return fetch(url.toString(), options);
 }
